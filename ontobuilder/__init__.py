e398a70bdfa8e7bc3152454957421195f38ca085
guyd1995 <57269082+guyd1995@users.noreply.github.com>
2022-02-18 15:33:08 +0200

    add jars and init file

---
+++ ontobuilder/__init__.py
@@ -0,0 +1,13 @@
+import os
+import jpype
+from jpype import JPackage
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+jpype.startJVM(classpath=[f"{dir_path}/jars/*"])
+_base_package = JPackage("ac").technion.iem.ontobuilder
+core = _base_package.core
+matching = _base_package.matching
+io = _base_package.io
+
+# Ontology = core.ontology.Ontology
+# Term = core.ontology.Term
