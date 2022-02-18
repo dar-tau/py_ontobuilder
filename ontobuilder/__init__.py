2d08742d4dc69afb022a0ee6e74f89ab90efb6ce
guyd1995 <57269082+guyd1995@users.noreply.github.com>
2022-02-18 17:11:23 +0200

    change pom

--- ontobuilder/__init__.py
+++ ontobuilder/__init__.py
@@ -3,11 +3,10 @@ import jpype
 from jpype import JPackage
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-jpype.startJVM(classpath=[f"{dir_path}/jars/*"])
+jpype.startJVM(classpath=[f"{dir_path}/jars/*/*", f"{dir_path}/jars/*/target/dependency/*"])
 _base_package = JPackage("ac").technion.iem.ontobuilder
 core = _base_package.core
 matching = _base_package.matching
 io = _base_package.io
-
-# Ontology = core.ontology.Ontology
-# Term = core.ontology.Term
+Ontology = core.ontology.Ontology
+Term = core.ontology.Term
